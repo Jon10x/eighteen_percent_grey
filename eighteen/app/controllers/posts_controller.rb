db8e9c2f6f01ce839b84bd63e2ea7f7396a77a4d
jon10x <admin@jonathanhiggins.ie>
2016-11-16 02:49:05 +0000

    fixed CRUD issue

--- eighteen/app/controllers/posts_controller.rb
+++ eighteen/app/controllers/posts_controller.rb
@@ -14,11 +14,11 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def create
    if @post = Post.create(post_params)
-    flash[:success] = "Your post has been created!"
-    redirect_to posts_path
-   else
-    flash.now[:alert] = "Your new post couldn't be created! Please check the form."
-    render :new
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      render :new
    end
   end
   
@@ -43,7 +43,7 @@ before_action :set_post, only: [:show, :edit, :update, :destroy]
   end
 
   private
-  
+    
   def post_params
     params.require(:post).permit(:image, :caption)
   end
