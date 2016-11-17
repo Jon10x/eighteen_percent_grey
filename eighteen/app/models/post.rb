24818f10dd352431f502551a5e63645ca04e92b3
jon10x <admin@jonathanhiggins.ie>
2016-11-17 02:15:31 +0000

    AJAX added to comments

--- eighteen/app/models/post.rb
+++ eighteen/app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ActiveRecord::Base
+ 
  validates :user_id, presence: true
  belongs_to :user
  validates :image, presence: true
  has_attached_file :image, styles: { :medium => "640x" }
  validates_attachment_content_type :image, :content_type =>
 /\Aimage\/.*\Z/
+ has_many :comments, dependent: :destroy
+ validates_length_of :caption, :minimum => 2, :maximum => 40, :allow_blank => true
 end
