65f439d18fb6f5377b0b89fa751113fd6f804592
jon10x <admin@jonathanhiggins.ie>
2016-11-18 02:21:56 +0000

    finished user profiles and comments

--- eighteen/app/models/user.rb
+++ eighteen/app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
     validates :user_name, presence: true, length: { minimum: 4,
     maximum: 16 }
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: { medium: '152x152#' }
+  validates_attachment_content_type :avatar, content_type:
+  /\Aimage\/.*\Z/
 end
