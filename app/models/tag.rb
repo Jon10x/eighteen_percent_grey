c918a642dc6332ab80fa51e1eb754d135b9e6ec3
jon10x <admin@jonathanhiggins.ie>
2016-12-03 16:09:40 +0000

    db fix #5

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,9 +6,12 @@ class Tag < ActiveRecord::Base
  #   self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
  # end
  
-  def self.tag_counts
-  Tag.select("tags.id, tags.name,count(taggings.tag_id) as count").
-    joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
-  end 
+ # def self.tag_counts
+  #Tag.select("tags.id, tags.name,count(taggings.tag_id) as count").
+  #  joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
+  # end 
 
+  def self.counts
+    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
+  end
 end
