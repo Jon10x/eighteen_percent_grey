15a6c002b78d6506e88d15b73a807435cc060df2
jon10x <admin@jonathanhiggins.ie>
2016-12-03 18:31:22 +0000

    heroku test version

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ActiveRecord::Base
   has_many :posts, through: :taggings
   
  def self.counts
-    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id, tags.name")
  end
  
   # def self.counts
